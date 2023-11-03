02d4ea6dfbffdd7fa0fc1d4650faf207995feb38
leslyndizey <l.ndizeye@alustudent.com>
2023-11-03 03:46:20 -0700

    everyone else

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from sys import argv
     from calculator_1 import add, sub, mul, div
-    import sys
-    j = {"+", "-", "*", "/"}
-    if len(sys.argv) != 4:
+    items = argv[1:]
+    count = len(items)
+    operator = ['+', '-', '*', '/']
+    if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    if sys.argv[2] not in j:
-        print("Unknown operator.Available operators: +, -, *, and /")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    result = 0
-    if sys.argv[2] == "+":
-        result = add(a, b)
-    elif sys.argv[2] == "-":
-        result = sub(a, b)
-    elif sys.argv[2] == "*":
-        result = mul(a, b)
-    elif sys.argv[2] == "/":
-        result = div(a, b)
-    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
+        exit(1)
+    elif argv[2] not in operator:
+        print('Unknown operator. Only: +, -, * and / available')
+        exit(1)
+    else:
+        if argv[2] == operator[0]:
+            a = int(items[0])
+            b = int(items[2])
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif argv[2] == operator[1]:
+            a = int(items[0])
+            b = int(items[2])
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+        elif argv[2] == operator[2]:
+            a = int(items[0])
+            b = int(items[2])
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
+        else:
+            a = int(items[0])
+            b = int(items[2])
+            print('{} / {} = {}'.format(a, b, div(a, b)))
