4010f6b1f0759f8350c7b52d3a48eaf10cbce050
leslyndizey <l.ndizeye@alustudent.com>
2023-11-30 02:37:58 -0800

    intro

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- insert into
-INSERT INTO first_table(id, name) VALUES(89, 'Holberton School');
+-- nserts a new row in the table first_table
+INSERT INTO first_table(id,name) VALUES(89, "Holberton School");
