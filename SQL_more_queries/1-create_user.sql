7b34612f8d4f0896b7e1e9e5f7c883066df66ff4
leslyndizey <l.ndizeye@alustudent.com>
2023-11-30 03:57:35 -0800

    all

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
---create user and grant all privileges
+-- cc
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
