49f4fe66f31a2888984eae09c673d5bd55928e3a
ndizeyelesly1234 <139573491+Leslyndizeye@users.noreply.github.com>
2024-02-11 17:51:56 +0100

    0-readme.js
    Signed-off-by: ndizeyelesly1234 <139573491+Leslyndizeye@users.noreply.github.com>

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
-const file = process.argv.slice(2)[0];
 
-fs.readFile(file, 'utf8', (err, contents) => {
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(contents);
+    console.log(data);
   }
 });
